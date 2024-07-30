46cab195fa798aa537a475b664f7e4285571bb48
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-07-30 18:41:27 -0300

    feat: add structure of basic bot commands

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
+
 require 'discordrb'
+require 'dotenv/load'
+
+TOKEN = ENV['SLASH_COMMAND_BOT_TOKEN'] || nil
+SERVER_ID = ENV['SLASH_COMMAND_BOT_SERVER_ID'] || nil
+
+bot = Discordrb::Bot.new(token: TOKEN, intents: [:server_messages])
+
+bot.register_application_command(:horas, 'Que horas são?', server_id: SERVER_ID)
+
+bot.application_command(:horas) do |event|
+  time = Time.now
+
+  event.respond(content: time)
+end
+
+bot.run
