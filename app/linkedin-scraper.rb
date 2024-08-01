804f996b2a2dc1170475dca9fb63595f4c0ead5d
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-01 19:44:26 -0300

    fix: add null verification on elements value

--- app/linkedin-scraper.rb
+++ app/linkedin-scraper.rb
@@ -19,10 +19,10 @@ class LinkedInScraper
     doc.css('ul.jobs-search__results-list > li').map do |job|
       {
         title: job.css('h3.base-search-card__title').text.strip || nil,
-        link: job.css('a.base-card__full-link').attr('href') || nil,
-        logo: job.css('div.search-entity-media img').attr('data-delayed-url') || nil,
+        link: job.css('a.base-card__full-link').attr('href')&.value || nil,
+        logo: job.css('div.search-entity-media img').attr('data-delayed-url')&.value || nil,
         company: job.css('h4.base-search-card__subtitle a').text.strip || nil,
-        company_link: job.css('h4.base-search-card__subtitle a').attr('href') || nil,
+        company_link: job.css('h4.base-search-card__subtitle a').attr('href')&.value || nil,
         location: job.css('span.job-search-card__location').text.strip || nil,
         posted_at: job.css('time.job-search-card__listdate--new').text.strip || nil
       }
