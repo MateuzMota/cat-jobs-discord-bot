3df504aa1634d84ffa230bb48fade2208dd3b98c
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-01 09:59:18 -0300

    chore: remove unused code

--- main.rb
+++ main.rb
@@ -4,11 +4,10 @@ require 'discordrb'
 require 'dotenv/load'
 
 TOKEN = ENV['SLASH_COMMAND_BOT_TOKEN'] || nil
-SERVER_ID = ENV['SLASH_COMMAND_BOT_SERVER_ID'] || nil
 
 bot = Discordrb::Bot.new(token: TOKEN, intents: [:server_messages])
 
-bot.register_application_command(:horas, 'Que horas são?', server_id: SERVER_ID)
+bot.register_application_command(:horas, 'Que horas são?')
 
 bot.application_command(:horas) do |event|
   time = Time.now
