f7a979350aae3f90c62ff803159d541d3abb9a28
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-01 20:03:27 -0300

    feat: add threading to run main function in parallel with DiscordDB

--- main.rb
+++ main.rb
@@ -13,32 +13,40 @@ require_relative 'app/json-file-handler'
 URL = ENV['LINKEDIN_URL_WITH_PARAMS']
 TOKEN = ENV['SLASH_COMMAND_BOT_TOKEN']
 WEBHOOK = ENV['DISCORD_CHANNEL_WEBHOOK']
-
 @bot = DiscordBot.new(TOKEN, WEBHOOK)
 
-if URL && TOKEN && WEBHOOK
-  scraper = LinkedInScraper.new(URL)
-  jobs = scraper.fetch_jobs
-  file_path = 'files/jobs_file.json'
-  json_file = JsonFileHandler.new(file_path)
+def main
+  if URL && TOKEN && WEBHOOK
+    scraper = LinkedInScraper.new(URL)
+    jobs = scraper.fetch_jobs
+    file_path = 'files/jobs_file.json'
+    json_file = JsonFileHandler.new(file_path)
 
-  File.zero?(file_path) && json_file.write_file(jobs)
+    File.zero?(file_path) && json_file.write_file(jobs)
 
-  json_file.read_file
+    json_file.read_file
 
-  jobs.each do |job|
-    existing_job = json_file.jobs_file[0].find { |job_from_json| job_from_json['link'] == job['link'] }
+    jobs.each do |job|
+      existing_job = json_file.jobs_file[0].find { |job_from_json| job_from_json['link'] == job['link'] }
 
-    if existing_job
-      puts 'já existe'
-    else
-      puts "Novo job: #{job}"
-      json_file.write_file(job)
-      # @bot.message_channel
+      if existing_job
+        puts 'já existe'
+      else
+        puts "Novo job: #{job}"
+        json_file.write_file(job)
+        # @bot.message_channel
+      end
     end
-  end
-  @bot.message_channel
+    @bot.message_channel
 
-else
-  puts 'URL, TOKEN ou WEBHOOK não definido.'
+  else
+    puts 'URL, TOKEN ou WEBHOOK não definido.'
+  end
+end
+
+Thread.new { @bot.run }
+
+loop do
+  main
+  sleep 10
 end
